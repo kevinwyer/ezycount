ca50b38b65efe7dd2ef07d0103c2accbc216c615
kevinwyer <kevinwyer@hotmail.ch>
2015-03-25 13:41:01 +0100

    Users

---
+++ ezycount/app/Test/Case/Controller/UsersControllerTest.php
@@ -0,0 +1,109 @@
+<?php
+App::uses('UsersController', 'Controller');
+
+/**
+ * UsersController Test Case
+ *
+ */
+class UsersControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.user'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+		$this->markTestIncomplete('testIndex not implemented.');
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+		$this->markTestIncomplete('testView not implemented.');
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+		$this->markTestIncomplete('testAdd not implemented.');
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+		$this->markTestIncomplete('testEdit not implemented.');
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+		$this->markTestIncomplete('testDelete not implemented.');
+	}
+
+/**
+ * testDIndex method
+ *
+ * @return void
+ */
+	public function testDIndex() {
+		$this->markTestIncomplete('testDIndex not implemented.');
+	}
+
+/**
+ * testDView method
+ *
+ * @return void
+ */
+	public function testDView() {
+		$this->markTestIncomplete('testDView not implemented.');
+	}
+
+/**
+ * testDAdd method
+ *
+ * @return void
+ */
+	public function testDAdd() {
+		$this->markTestIncomplete('testDAdd not implemented.');
+	}
+
+/**
+ * testDEdit method
+ *
+ * @return void
+ */
+	public function testDEdit() {
+		$this->markTestIncomplete('testDEdit not implemented.');
+	}
+
+/**
+ * testDDelete method
+ *
+ * @return void
+ */
+	public function testDDelete() {
+		$this->markTestIncomplete('testDDelete not implemented.');
+	}
+
+}
---
+++ ezycount/app/Test/Fixture/UserFixture.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * UserFixture
+ *
+ */
+class UserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'first_name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'last_name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'country' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'is_activated' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'is_admin' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'disabled' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 1, 'unsigned' => false),
+		'didTour' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 1, 'unsigned' => false),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'email' => 'Lorem ipsum dolor sit amet',
+			'password' => 'Lorem ipsum dolor sit amet',
+			'first_name' => 'Lorem ipsum dolor sit amet',
+			'last_name' => 'Lorem ipsum dolor sit amet',
+			'country' => 'Lorem ipsum dolor sit amet',
+			'is_activated' => 1,
+			'is_admin' => 1,
+			'created' => '2015-03-25 13:28:26',
+			'disabled' => 1,
+			'didTour' => 1
+		),
+	);
+
+}
