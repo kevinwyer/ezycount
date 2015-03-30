82a8ad03b4aab30ddb188d0b6868ea7e1705e221
kevinwyer <kevinwyer@hotmail.ch>
2015-03-30 19:20:57 +0200

    merge resolve

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ed/005182d99dd3001417dfb23f844b5ace
@@ -0,0 +1,79 @@
+<div class="users view">
+<h2><?php echo __('User'); ?></h2>
+	<dl>
+		<dt><?php echo __('Id'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['id']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Email'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['email']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Password'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['password']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Title'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['title']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('First Name'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['first_name']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Last Name'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['last_name']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Country'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['country']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Is Activated'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['is_activated']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Is Admin'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['is_admin']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Created'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['created']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Language'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['language']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('Disabled'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['disabled']); ?>
+			&nbsp;
+		</dd>
+		<dt><?php echo __('DidTour'); ?></dt>
+		<dd>
+			<?php echo h($user['User']['didTour']); ?>
+			&nbsp;
+		</dd>
+	</dl>
+</div>
+<div class="actions">
+	<h3><?php echo __('Actions'); ?></h3>
+	<ul>
+		<li><?php echo $this->Html->link(__('Edit User'), array('action' => 'edit', $user['User']['id'])); ?> </li>
+		<li><?php echo $this->Form->postLink(__('Delete User'), array('action' => 'delete', $user['User']['id']), array(), __('Are you sure you want to delete # %s?', $user['User']['id'])); ?> </li>
+		<li><?php echo $this->Html->link(__('List Users'), array('action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New User'), array('action' => 'add')); ?> </li>
+	</ul>
+</div>
---
+++ ezycount/app/Test/Fixture/CompanyFixture.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * CompanyFixture
+ *
+ */
+class CompanyFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'index', 'comment' => 'represent the user id of the creator'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'number' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'street' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'zip' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 5, 'unsigned' => false),
+		'city' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'country' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'phone' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'website' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'type' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'first_accounting_year' => array('type' => 'date', 'null' => false, 'default' => null),
+		'closing_date' => array('type' => 'date', 'null' => false, 'default' => null),
+		'blocking_date' => array('type' => 'date', 'null' => false, 'default' => null),
+		'currency' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 5, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'vat_registered' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'vat_model' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => '1 = effective rate, 0 = net tax rate'),
+		'vat_both' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'vat_number' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'starting_vat' => array('type' => 'date', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'expiration_date' => array('type' => 'date', 'null' => false, 'default' => null),
+		'current_step' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 4, 'unsigned' => false, 'comment' => 'represent the current step for the creation, there are 5 steps where 0 is the first and 4 is the final step'),
+		'test' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'logo' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'background_color' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 6, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'disabled' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'canton' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 55, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'user_id' => array('column' => 'user_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'number' => 'Lorem ipsum dolor sit amet',
+			'street' => 'Lorem ipsum dolor sit amet',
+			'zip' => 1,
+			'city' => 'Lorem ipsum dolor sit amet',
+			'country' => 'Lorem ipsum dolor sit amet',
+			'phone' => 'Lorem ipsum dolor sit amet',
+			'email' => 'Lorem ipsum dolor sit amet',
+			'website' => 'Lorem ipsum dolor sit amet',
+			'type' => 'Lorem ipsum dolor sit amet',
+			'first_accounting_year' => '2015-03-30',
+			'closing_date' => '2015-03-30',
+			'blocking_date' => '2015-03-30',
+			'currency' => 'Lor',
+			'vat_registered' => 1,
+			'vat_model' => 1,
+			'vat_both' => 1,
+			'vat_number' => 'Lorem ipsum dolor sit amet',
+			'starting_vat' => '2015-03-30',
+			'created' => '2015-03-30 14:22:56',
+			'expiration_date' => '2015-03-30',
+			'current_step' => 1,
+			'test' => 1,
+			'logo' => 'Lorem ipsum dolor sit amet',
+			'background_color' => 'Lore',
+			'disabled' => 1,
+			'canton' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
