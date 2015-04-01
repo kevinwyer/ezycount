27666fcb1d44d531b49e0b43b2a93f0fad7b7d5d
EvelynHischier <evelyn_hischier@hotmail.com>
2015-04-01 15:37:50 +0200

    correcting search

--- ezycount/app/Controller/UsersController.php
+++ ezycount/app/Controller/UsersController.php
@@ -1,63 +1,60 @@
 <?php
 App::uses ( 'AppController', 'Controller' );
 class UsersController extends AppController {
-	
 	public $components = array (
 			'Paginator',
 			'Session' 
 	);
-	
-	public function deleteSession(){
-	
+	public function deleteSession() {
+		
 		// check if a name was stored in the session
-		// if existing remove it 
+		// if existing remove it
 		if ($this->Session->check ( 'search_name' ))
-			$this->Session->destroy('search_name');
-	
-		// check if a mail was stored in the session
-		// if existing remove it
+			$this->Session->destroy ( 'search_name' );
+			
+			// check if a mail was stored in the session
+			// if existing remove it
 		if ($this->Session->check ( 'search_email' ))
-			$this->Session->destroy('search_email');
-		
-		// check if a radiobutton (and / or)  was stored in the session
-		// if existing remove it
+			$this->Session->destroy ( 'search_email' );
+			
+			// check if a radiobutton (and / or) was stored in the session
+			// if existing remove it
 		if ($this->Session->check ( 'select_condition' ))
-			$this->Session->destroy('select_condition');
-		
-		// reload the page
-		header("location:/Git/ezycount/ezycount/users"); 
-		exit();
-	
+			$this->Session->destroy ( 'select_condition' );
+			
+			// reload the page
+		header ( "location:/Git/ezycount/ezycount/users" );
+		exit ();
 	}
-	
 	private function searchFieldsUsed() {
-		
-
 		
 		// check if the search function was used
 		if (isset ( $_POST ["search_name"] ) && isset ( $_POST ["search_email"] )) {
 			
 			// check if the search fields are NOT containing something
-			if ($_POST ["search_email"] == "" && $_POST ["search_name"] == "") {
-				
+			if ($_POST ["search_name"] == "")
+				$this->Session->destroy ( 'search_name' );
 				// nothing in the input fields
-				return false;
-			} else {
-				
-				// only add the session variable if a name was searched
-				if ($_POST ["search_name"] != "")
-					$this->Session->write ( 'search_name', $_POST ["search_name"] );
+			
+			if ($_POST ["search_email"] == "") {
+				$this->Session->destroy ( 'search_email' );
+				// nothing in the input fields
+			}
+			
+			// only add the session variable if a name was searched
+			if ($_POST ["search_name"] != "")
+				$this->Session->write ( 'search_name', $_POST ["search_name"] );
 				
 				// only add the session variable if a mail was searched
-				if ($_POST ["search_email"] != "")
-					$this->Session->write ( 'search_email', $_POST ["search_email"] );
-				
-				if ($_POST ["search_condition"] != "")
-					$this->Session->write('select_condition', $_POST["search_condition"]);
-				
-				return true;
-			}
+			if ($_POST ["search_email"] != "")
+				$this->Session->write ( 'search_email', $_POST ["search_email"] );
+			
+			if ($_POST ["search_condition"] != "")
+				$this->Session->write ( 'select_condition', $_POST ["search_condition"] );
+			
+			return true;
 		}
+		
 		return false;
 	}
 	public function index() {
@@ -80,19 +77,17 @@ class UsersController extends AppController {
 			);
 			// Check if there's something in the session
 		} else if ($this->Session->check ( 'session' )) {
-
+			
 			// Put the value as defaultLimit when the page refreshes
-			$defaultLimit =  $this->Session->read ( 'session' );
+			$defaultLimit = $this->Session->read ( 'session' );
 			
 			// Display with session value
 			$this->paginate = array (
 					'User' => array (
-							'limit' => $defaultLimit
+							'limit' => $defaultLimit 
 					) 
 			);
-			
-		} 
-		else {
+		} else {
 			// Display with default value
 			$this->paginate = array (
 					'User' => array (
@@ -101,35 +96,56 @@ class UsersController extends AppController {
 			);
 		}
 		
-		$this->searchFieldsUsed(); 
+		$this->searchFieldsUsed ();
 		
 		// check if session containing a search select
-		if (! $this->Session->check ( 'select_condition' )){
+		if (! $this->Session->check ( 'select_condition' )) {
 			// default
-			$this->Session->write('select_condition', 'OR');
+			$this->Session->write ( 'select_condition', 'OR' );
 		}
 		
-		
 		// display result of search
-		if ($this->Session->check ( 'search_email' ) || $this->Session->check ( 'search_name' ) ) {
+		if ($this->Session->check ( 'search_email' ) || $this->Session->check ( 'search_name' )) {
 			
 			
+			// prepare statement OR
+			$conditionOR = ' where ( ezycount_users.first_name LIKE "' . $this->Session->read ( 'search_name' ) . '"' . 
+			' OR  ' . ' ezycount_users.last_name LIKE "' . $this->Session->read ( 'search_name' ) . '")' . 
+			' OR ' . ' ezycount_users.email LIKE "' . $this->Session->read ( 'search_email' ) . '"  
+						group by ezycount_users.id
+					';
+			
+			if ($this->Session->read ( 'search_name' ) != "" && $this->Session->read ( 'search_email' ) != "") {
+				// prepare statement AND
+				// when email and firstname / lastname are search criterias
+				$conditionAND = 
+						$this->Session->read ( 'search_name' ) == "" ? '' : ' where 
+								( ezycount_users.first_name LIKE "' . $this->Session->read ( 'search_name' ) . '"' . 
+								' OR  ' . 
+								' ezycount_users.last_name LIKE "' . $this->Session->read ( 'search_name' ) . '" )' . 
+						$this->Session->read ( 'search_email' ) == "" ? '' : ' and ezycount_users.email LIKE "' . $this->Session->read ( 'search_email' ) . '"  
+						group by ezycount_users.id ';
+			} else {
+				// prepare statement AND
+				// when either email or firstname / lastname are search criterias
+				$conditionAND = 
+						$this->Session->read ( 'search_name' ) == "" ? '' : ' where ( 
+								ezycount_users.first_name LIKE "' . $this->Session->read ( 'search_name' ) . '"' . 
+								' OR  ' . 
+								' ezycount_users.last_name LIKE "' . $this->Session->read ( 'search_name' ) . '" )' . 
+						$this->Session->read ( 'search_email' ) == "" ? '' : ' where ezycount_users.email LIKE "' . $this->Session->read ( 'search_email' ) . '" 
+						group by ezycount_users.id';
+			}
 			// display for test reasons the Session content
 			echo 'Search in use <br/>';
-			echo '<br/> mail ' . $this->Session->read('search_email');
-			echo '<br/> name '  . $this->Session->read('search_name');
+			echo '<br/> mail ' . $this->Session->read ( 'search_email' );
+			echo '<br/> name ' . $this->Session->read ( 'search_name' );
 			
 			// query the right information
 			$this->paginate = array (
 					'User' => array (
-							'conditions' => 
-								(' AND ( ezycount_users.first_name LIKE "' .  $this->Session->read ( 'search_name' ) . '"') . 
-									' OR ' . 
-								' ezycount_users.last_name LIKE "' . $this->Session->read ( 'search_name' ) . '" )' .
-									' '. $this->Session->read('select_condition'). '  ' . 
-								' ezycount_users.email LIKE "' .  $this->Session->read ( 'search_email' ) .
-								 '"   ',
-								'limit' => $defaultLimit
+							'conditions' => ($this->Session->read ( 'select_condition' ) == "AND" ? $conditionAND : $conditionOR),
+							'limit' => $defaultLimit 
 					) 
 			);
 		}
